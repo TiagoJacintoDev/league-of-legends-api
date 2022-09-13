afe841fe478cb686bd6d7a45eaa54d9e99bda4ab
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-09-13 16:13:19 +0200

    Fetch using a Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useFetch } from './hooks/useFetch';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://euw1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=RGAPI-c7de2433-a44f-4297-97e7-def4301180e3'
-      )
-      .then(res => setData(res.data));
-  }, []);
-
+  const { data } = useFetch(
+    'https://euw1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=RGAPI-c7de2433-a44f-4297-97e7-def4301180e3'
+  );
   return (
     <>
       {data.map(player => (
