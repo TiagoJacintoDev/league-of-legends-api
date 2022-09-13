8ec9d35c62c3150eac4271540a5d1ce8358042c7
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-09-13 16:39:33 +0200

    Catch Fetch Errors && Display Them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useFetch } from './hooks/useFetch';
 
 function App() {
-  const { data: players, isFetching } = useFetch(
+  const {
+    data: players,
+    isFetching,
+    error,
+  } = useFetch(
     'https://euw1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=RGAPI-c7de2433-a44f-4297-97e7-def4301180e3'
   );
   return (
     <>
+      {error && <h1>{error}</h1>}
       {isFetching && <h1>Loading...</h1>}
       {players?.map(player => (
         <>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,17 +4,20 @@ import { useEffect, useState } from 'react';
 export function useFetch(url) {
   const [data, setData] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     (async function getData() {
       try {
         const res = await axios.get(url);
         setData(res.data);
+      } catch (error) {
+        setError(error.message);
       } finally {
         setIsFetching(false);
       }
     })();
   }, []);
 
-  return { data, isFetching };
+  return { data, isFetching, error };
 }
